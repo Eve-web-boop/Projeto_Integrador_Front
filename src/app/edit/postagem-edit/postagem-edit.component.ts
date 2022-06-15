417f2f148246a1744bf121093eb93250e7d22c77
Eve-web-boop <sintiaeveline@gmail.com>
2022-06-14 23:40:57 -0300

    Implementação do put e delete de postagens

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categoria } from 'src/app/model/Categoria';
+import { Postagem } from 'src/app/model/Postagem';
+import { CategoriaService } from 'src/app/service/categoria.service';
+import { PostagemService } from 'src/app/service/postagem.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-postagem-edit',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostagemEditComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem
 
-  ngOnInit(): void {
+  tema: Categoria = new Categoria()
+  listaTema: Categoria[]
+  idTema: number
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postagemService: PostagemService,
+    private categoriaService: CategoriaService
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdPostagem(id)
+
+  }
+
+  findByIdPostagem(id: number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem) =>{
+      this.postagem = resp
+    })
+  }
+
+  findByIdTema(){
+    this.categoriaService
+  }
+
+  atualizar(){
+    this.tema.id = this.idTema
+    this.postagem.categoria = this.tema
+
+    this.postagemService.putPostagem(this.postagem).subscribe((resp: Postagem) =>{
+      this.postagem = resp
+      alert('Postagem atualizada com sucesso!')
+      this.router.navigate(['/inicio'])
+    })
   }
 
 }
